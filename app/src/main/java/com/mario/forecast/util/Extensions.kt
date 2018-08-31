b81dca1780a055a5d5406c9399b9df6cd88b9ea2
Mario Feles dos Santos Junior <mario_feles@live.com>
2018-08-31 12:28:47 -0300

    update icons

--- app/src/main/java/com/mario/forecast/util/Extensions.kt
+++ app/src/main/java/com/mario/forecast/util/Extensions.kt
@@ -1,22 +1,23 @@
 package com.mario.forecast.util
 
 import android.content.Context
+import androidx.core.content.ContextCompat
 import com.mario.forecast.R
 import java.text.SimpleDateFormat
 import java.util.*
 
-fun Date.formataDate() = SimpleDateFormat("EEEE, MMMM, YYYY").format(this)
+fun Date.formataDate() = SimpleDateFormat("EEEE, MMMM, yyyy",Locale.getDefault()).format(this)
 
 
 fun Context.findIcon(icon: String) =
         when (icon) {
-            "01d", "01n" -> resources.getDrawable(R.drawable.art_clear)
-            "02d", "02n" -> resources.getDrawable(R.drawable.art_light_clouds)
-            "03d", "03n" -> resources.getDrawable(R.drawable.art_clouds)
-            "04d", "04n" -> resources.getDrawable(R.drawable.art_fog)
-            "10d", "10n" -> resources.getDrawable(R.drawable.art_rain)
-            "9d", "9n" -> resources.getDrawable(R.drawable.art_light_rain)
-            "11d", "11n" -> resources.getDrawable(R.drawable.art_storm)
-            "13d", "13n" -> resources.getDrawable(R.drawable.art_snow)
-            else -> resources.getDrawable(R.drawable.art_clear)
+            "01d", "01n" -> ContextCompat.getDrawable(this, R.drawable.art_clear)
+            "02d", "02n" -> ContextCompat.getDrawable(this, R.drawable.art_light_clouds)
+            "03d", "03n" -> ContextCompat.getDrawable(this, R.drawable.art_clouds)
+            "04d", "04n" -> ContextCompat.getDrawable(this, R.drawable.art_fog)
+            "10d", "10n" -> ContextCompat.getDrawable(this, R.drawable.art_rain)
+            "9d", "9n" -> ContextCompat.getDrawable(this, R.drawable.art_light_rain)
+            "11d", "11n" -> ContextCompat.getDrawable(this, R.drawable.art_storm)
+            "13d", "13n" -> ContextCompat.getDrawable(this, R.drawable.art_snow)
+            else -> ContextCompat.getDrawable(this, R.drawable.art_clear)
         }
